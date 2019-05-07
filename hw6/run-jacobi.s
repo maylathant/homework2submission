826caf955ad0906795307901cdfa8e934f999ef4
Anthony Maylath <anthonymaylath@Anthonys-MacBook-Pro-2.local>
2019-05-07 14:00:22 -0400

    Powers of 4 to slurm

--- hw6/run-jacobi.s
+++ hw6/run-jacobi.s
@@ -18,6 +18,23 @@ module load openmpi/intel/3.1.3
 
 make
 
+mpirun -np 1 ./jacobi_mpi 100 100
+mpirun -np 1 ./jacobi_mpi 100 1000
+mpirun -np 1 ./jacobi_mpi 100 10000
+
+mpirun -np 4 ./jacobi_mpi 400 100
+mpirun -np 4 ./jacobi_mpi 400 1000
+mpirun -np 4 ./jacobi_mpi 400 10000
+
+
+mpirun -np 16 ./jacobi_mpi 1600 100
+mpirun -np 16 ./jacobi_mpi 1600 1000
+mpirun -np 16 ./jacobi_mpi 1600 10000
+
+mpirun -np 64 ./jacobi_mpi 6400 100
+mpirun -np 64 ./jacobi_mpi 6400 1000
+mpirun -np 64 ./jacobi_mpi 6400 10000
+
 mpirun -np 256 ./jacobi_mpi 25600 100
 mpirun -np 256 ./jacobi_mpi 25600 1000
 mpirun -np 256 ./jacobi_mpi 25600 10000
