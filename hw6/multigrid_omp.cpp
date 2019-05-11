0f5d731884be06822dd68dfc3193446f4963f237
Anthony Maylath <anthonymaylath@Anthonys-MacBook-Pro-2.local>
2019-05-10 23:35:26 -0400

    Add omp multigrid

--- hw6/multigrid_omp.cpp
+++ hw6/multigrid_omp.cpp
@@ -4,29 +4,34 @@
  * ITER: max number of V-cycle iterations
  * S-STEPS: number of Jacobi smoothing steps; optional
  * Author: Georg Stadler, April 2017
- * MPI Part by Anthony Maylath, May 2019
  */
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-#include <mpi.h>
-#include <stdlib.h>
 #include "utils.h"
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 /* compuate norm of residual */
 double compute_norm(double *u, int N)
 {
-  int i; 
-  double norm = 0.0; double gnorm = 0.0;
+  int i;
+  double norm = 0.0;
+  #ifdef _OPENMP
+  #pragma omp parallel for private(i), shared(u,N), reduction(+:norm)
+  #endif
   for (i = 0; i <= N; i++)
     norm += u[i] * u[i];
-  MPI_Allreduce(&norm, &gnorm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-  return sqrt(gnorm);
+  return sqrt(norm);
 }
 
 /* set vector to zero */
 void set_zero (double *u, int N) {
   int i;
+  #ifdef _OPENMP
+  #pragma omp parallel for private(i), shared(N)
+  #endif
   for (i = 0; i <= N; i++)
     u[i] = 0.0;
 }
@@ -44,6 +49,9 @@ void output_to_screen (double *u, int N) {
 */
 void coarsen(double *uf, double *uc, int N) {
   int ic;
+  #ifdef _OPENMP
+  #pragma omp parallel for private(ic), shared(N,uc,uf)
+  #endif
   for (ic = 1; ic < N/2; ++ic)
     uc[ic] = 0.5 * uf[2*ic] + 0.25 * (uf[2*ic-1]+uf[2*ic+1]);
 }
@@ -56,6 +64,9 @@ void refine_and_add(double *u, double *uf, int N)
 {
   int i;
   uf[1] += 0.5 * (u[0] + u[1]);
+  #ifdef _OPENMP
+  #pragma omp parallel for private(i), shared(N,uf,u)
+  #endif
   for (i = 1; i < N; ++i) {
     uf[2*i] += u[i];
     uf[2*i+1] += 0.5 * (u[i] + u[i+1]);
@@ -63,15 +74,14 @@ void refine_and_add(double *u, double *uf, int N)
 }
 
 /* compute residual vector */
-void compute_residual(double *u, double *rhs, double *res, int N, double invhsq, int rank = 0, int p = 0)
+void compute_residual(double *u, double *rhs, double *res, int N, double invhsq)
 {
-  int i = 0, end = N;
-  if(rank==0){i = 1;}
-  if(rank==p-1){end = N-1;}
-  for (; i < end; i++){
-    res[rank*N+i] = (rhs[rank*N+i] - (2.*u[i+1] - u[i] - u[i+2]) * invhsq);
-  }
-
+  int i;
+  #ifdef _OPENMP
+  #pragma omp parallel for private(i), shared(u,invhsq,rhs,N)
+  #endif
+  for (i = 1; i < N; i++)
+    res[i] = (rhs[i] - (2.*u[i] - u[i-1] - u[i+1]) * invhsq);
 }
 
 
@@ -91,14 +101,18 @@ void jacobi(double *u, double *rhs, int N, double hsq, int ssteps)
 {
   int i, j;
   /* Jacobi damping parameter -- plays an important role in MG */
-  double omega = 2./3.;
+  double omega = 2./3., utemp; //Add utemp to avoid a memory read
   double *unew = (double*) malloc(sizeof(double) * (N+1));
   for (i=0; i < (N+1); ++i) {
     unew[i] = 0.;
   }
   for (j = 0; j < ssteps; ++j) {
+    #ifdef _OPENMP
+    #pragma omp parallel for private(i,utemp), shared(u,omega,hsq,rhs)
+    #endif
     for (i = 1; i < N; i++){
-      unew[i]  = u[i] +  omega * 0.5 * (hsq*rhs[i] + u[i - 1] + u[i + 1] - 2*u[i]);
+      utemp = u[i];
+      unew[i]  = utemp +  omega * 0.5 * (hsq*rhs[i] + u[i - 1] + u[i + 1] - 2*utemp);
     }
     memcpy(u, unew, (N+1)*sizeof(double));
   }
@@ -108,39 +122,34 @@ void jacobi(double *u, double *rhs, int N, double hsq, int ssteps)
 
 int main(int argc, char * argv[])
 {
+  int i, Nfine, l, iter, max_iters, levels, ssteps = 3, num_threads = 1;
 
-  MPI_Init(&argc, &argv);
-  int i, Nfine, l, iter, max_iters, levels, ssteps = 3;
-
-  if (argc < 3 || argc > 4) {
+  if (argc < 3 || argc > 5) {
     fprintf(stderr, "Usage: ./multigrid_1d Nfine maxiter [s-steps]\n");
     fprintf(stderr, "Nfine: # of intervals, must be power of two number\n");
     fprintf(stderr, "s-steps: # jacobi smoothing steps (optional, default is 3)\n");
+    fprintf(stderr, "num_threads: # of openmp threads\n");
     abort();
   }
   sscanf(argv[1], "%d", &Nfine);
   sscanf(argv[2], "%d", &max_iters);
-  if (argc > 3)
+  if (argc > 3){
     sscanf(argv[3], "%d", &ssteps);
-
-  int world_size, p_data, world_rank;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-  /* compute number of unknowns handled by each process */
-  int lN = Nfine / world_size;
-  if ((Nfine % world_size != 0) && world_rank == 0 ) {
-    printf("N: %d, local N: %d\n", Nfine, lN);
-    printf("Exiting. N must be a multiple of p\n");
-    abort();
+    if(argc > 4)
+      sscanf(argv[4], "%d", &num_threads);
   }
 
+
   /* compute number of multigrid levels */
-  levels = floor(log2(lN));
+  levels = floor(log2(Nfine));
   printf("Multigrid Solve using V-cycles for -u'' = f on (0,1)\n");
   printf("Number of intervals = %d, max_iters = %d\n", Nfine, max_iters);
   printf("Number of MG levels: %d \n", levels);
+
+  //Set omp threads
+  #ifdef _OPENMP
+  omp_set_num_threads(num_threads);
+  #endif
 
   /* timing */
   Timer t;
@@ -149,67 +158,61 @@ int main(int argc, char * argv[])
   /* Allocation of vectors, including left and right bdry points */
   double *u[levels], *rhs[levels];
   /* N, h*h and 1/(h*h) on each level */
-  int *N = (int*) malloc(sizeof(int) * levels); //Local part
-  int *Nfull = (int*) malloc(sizeof(int) * levels); //Full part
+  int *N = (int*) malloc(sizeof(int) * levels);
   double *invhsq = (double* ) malloc(sizeof(double) * levels);
   double *hsq = (double* ) malloc(sizeof(double) * levels);
-  double * res = (double *) calloc(Nfine+1, sizeof(double));
+  double * res = (double *) malloc(sizeof(double) * (Nfine+1));
   for (l = 0; l < levels; ++l) {
-    N[l] = lN / (int) pow(2,l) + 2; //Add two for ghost points
-    Nfull[l] = Nfine / (int) pow(2,l); //Dont add two for ghost points
-    double h = 1.0 / (Nfull[l]);
+    N[l] = Nfine / (int) pow(2,l);
+    double h = 1.0 / N[l];
     hsq[l] = h * h;
-    printf("MG level %2d, N = %8d, Rank = %d\n", l, N[l],world_rank);
+    printf("MG level %2d, N = %8d\n", l, N[l]);
     invhsq[l] = 1.0 / hsq[l];
-    u[l]    = (double *) malloc(N[l]*sizeof(double*));
-    for (int i=0; i<N[l]; ++i) {
+    u[l]    = (double *) malloc(sizeof(double) * (N[l]+1));
+    for (int i=0; i<N[l]+1; ++i) {
       u[l][i]=0.;
     }
-    rhs[l] = (double *) malloc(sizeof(double) * Nfull[l]+1); 
+    rhs[l] = (double *) malloc(sizeof(double) * (N[l]+1));
   }
   /* rhs on finest mesh */
-  for (i = 0; i <= Nfull[0]; ++i) {
+  for (i = 0; i <= N[0]; ++i) {
     rhs[0][i] = 1.0;
   }
-
+  /* set boundary values (unnecessary if calloc is used) */
+  u[0][0] = u[0][N[0]] = 0.0;
   double res_norm, res0_norm, tol = 1e-6;
 
-  // for(int i = 0; i<Nfine+1; i++){
-  //   printf("res[%d] = %f on rank %d\n", i,res[i], world_rank);
-  // }
-
   /* initial residual norm */
-  compute_residual(u[0], rhs[0], res, N[0]-2, invhsq[0], world_rank, world_size);
-  res_norm = res0_norm = compute_norm(res, Nfull[0]);
+  compute_residual(u[0], rhs[0], res, N[0], invhsq[0]);
+  res_norm = res0_norm = compute_norm(res, N[0]);
   printf("Initial Residual: %f\n", res0_norm); 
 
+  for (iter = 0; iter < max_iters && res_norm/res0_norm > tol; iter++) {
+    /* V-cycle: Coarsening */
+    for (l = 0; l < levels-1; ++l) {
+      /* pre-smoothing and coarsen */
+      jacobi(u[l], rhs[l], N[l], hsq[l], ssteps);
+      compute_and_coarsen_residual(u[l], rhs[l], rhs[l+1], N[l], invhsq[l]);
+      /* initialize correction for solution with zero */
+      set_zero(u[l+1],N[l+1]);
+    }
+    /* V-cycle: Solve on coarsest grid using many smoothing steps */
+    jacobi(u[levels-1], rhs[levels-1], N[levels-1], hsq[levels-1], 50);
 
-  // for (iter = 0; iter < max_iters && res_norm/res0_norm > tol; iter++) {
-  //   /* V-cycle: Coarsening */
-  //   for (l = 0; l < levels-1; ++l) {
-  //     /* pre-smoothing and coarsen */
-  //     jacobi(u[l], rhs[l], N[l], hsq[l], ssteps);
-  //     compute_and_coarsen_residual(u[l], rhs[l], rhs[l+1], N[l], invhsq[l]);
-  //     /* initialize correction for solution with zero */
-  //     set_zero(u[l+1],N[l+1]);
-  //   }
-  //   /* V-cycle: Solve on coarsest grid using many smoothing steps */
-  //   jacobi(u[levels-1], rhs[levels-1], N[levels-1], hsq[levels-1], 50);
+    /* V-cycle: Refine and correct */
+    for (l = levels-1; l > 0; --l) {
+      /* refine and add to u */
+      refine_and_add(u[l], u[l-1], N[l]);
+      /* post-smoothing steps */
+      jacobi(u[l-1], rhs[l-1], N[l-1], hsq[l-1], ssteps);
+    }
 
-  //   /* V-cycle: Refine and correct */
-  //   for (l = levels-1; l > 0; --l) {
-  //     /* refine and add to u */
-  //     refine_and_add(u[l], u[l-1], N[l]);
-  //     /* post-smoothing steps */
-  //     jacobi(u[l-1], rhs[l-1], N[l-1], hsq[l-1], ssteps);
-  //   }
-
-  //   if (0 == (iter % 1)) {
-  //     compute_residual(u[0], rhs[0], res, N[0], invhsq[0]);
-  //     res_norm = compute_norm(res, N[0]);
-  //     printf("[Iter %d] Residual norm: %2.8f\n", iter, res_norm);
-  //   }
-  // }
+    if (0 == (iter % 1)) {
+      compute_residual(u[0], rhs[0], res, N[0], invhsq[0]);
+      res_norm = compute_norm(res, N[0]);
+      printf("[Iter %d] Residual norm: %2.8f\n", iter, res_norm);
+    }
+  }
 
   /* Clean up */
   free (hsq);
@@ -224,6 +227,5 @@ int main(int argc, char * argv[])
   /* timing */
   double elapsed = t.toc();
   printf("Time elapsed is %f seconds.\n", elapsed);
-  MPI_Finalize();
   return 0;
 }
